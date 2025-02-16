6d035c5703b73e99174c6f212d3acb1c14ed8dbf
Bjorn Pagen <11238136+bjornpagen@users.noreply.github.com>
2025-02-16 16:33:39 -0600

    experimental flags

--- next.config.js
+++ next.config.js
@@ -5,6 +5,12 @@
 import "./src/env.js"
 
 /** @type {import("next").NextConfig} */
-const config = {}
+const config = {
+  experimental: {
+    ppr: true,
+    dynamicIO: true,
+    reactCompiler: true
+  }
+}
 
 export default config
