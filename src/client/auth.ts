1161a131b0f7beb7413facf592248888a99511ac
Bjorn Pagen <11238136+bjornpagen@users.noreply.github.com>
2025-02-16 20:44:38 -0600

    console.log

--- src/client/auth.ts
+++ src/client/auth.ts
@@ -2,6 +2,9 @@
 
 import { env } from "@/env"
 import { createAuthClient } from "better-auth/react"
+
+console.log(env.NEXT_PUBLIC_VERCEL_URL)
+
 export const authClient = createAuthClient({
   baseURL: env.NEXT_PUBLIC_VERCEL_URL
     ? `https://${env.NEXT_PUBLIC_VERCEL_URL}`
