768b0201882b56edd49ba61c17a4c34999811800
Bjorn Pagen <11238136+bjornpagen@users.noreply.github.com>
2025-02-17 17:59:21 -0600

    no barns in finland

--- src/server/db/script/drop.ts
+++ src/server/db/script/drop.ts
@@ -5,8 +5,9 @@ loadEnvConfig(projectDir)
 
 import { db } from "@/server/db"
 import { sql } from "drizzle-orm"
+import { schema } from "@/server/db/schema"
 
-db.execute(sql`DROP SCHEMA IF EXISTS dm2stay CASCADE`)
+db.execute(sql.raw(`DROP SCHEMA IF EXISTS ${schema.schemaName} CASCADE`))
   .then(() => process.exit(0))
   .catch((error) => {
     console.error("Error dropping schema:", error)
