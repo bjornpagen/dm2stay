4b2e90ab998c380fcc8d357843cd89adf6f20bd8
Bjorn Pagen <11238136+bjornpagen@users.noreply.github.com>
2025-02-16 19:01:44 -0600

    update drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,18 @@
 import type { Config } from "drizzle-kit"
+import { loadEnvConfig } from "@next/env"
 
-import { env } from "@/env"
+const projectDir = process.cwd()
+loadEnvConfig(projectDir)
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not set")
+}
 
 export default {
   schema: "./src/server/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: env.DATABASE_URL
+    url: process.env.DATABASE_URL
   },
   schemaFilter: ["dm2stay"]
 } satisfies Config
