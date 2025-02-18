0ed1b61e2db4d292672a507762a732783b1a2aa6
Bjorn Pagen <11238136+bjornpagen@users.noreply.github.com>
2025-02-17 20:01:51 -0600

    disable cookie cache

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -19,12 +19,6 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true
   },
-  session: {
-    cookieCache: {
-      enabled: true,
-      maxAge: 5 * 60
-    }
-  },
   advanced: {
     generateId: false
   }
